8c9a78cf88c6b8292871f82d926afc76581baabe
Rafaela Silva <rafams.rafa@gmail.com>
2023-07-10 22:04:45 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+let nome: string = "João"
+let nome2: string
+nome2 = "maria"
+
+let idade: number = 27
+let idade2: number
+idade2 = 27
+
+let ativo: boolean = true
+let ativo2: boolean
+ativo2 = false
+
+let usuario: object = {
+    id: 1,
+    email: 'teste@ativo.com',
+    senha: 123
+}
+
+
+
